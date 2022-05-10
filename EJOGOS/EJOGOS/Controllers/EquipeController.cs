4ed66378a57a8588acc39c663d7d39ce22632b98
Evandro Vidéo de Souza Júnior <evandrosouzabr@gmail.com>
2022-05-09 21:30:34 -0400

    Send EJOGOS project
    Send EJOGOS project to Github.

---
+++ EJOGOS/EJOGOS/Controllers/EquipeController.cs
@@ -0,0 +1,24 @@
+﻿using EJOGOS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EJOGOS.Controllers
+{
+    public class EquipeController : Controller
+    {
+        //ActionResult representam os vários códigos HTTP,
+
+        //Códigos HTTP - https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
+
+        Equipe equipeModel = new Equipe();
+
+        public IActionResult Index()
+        {
+            //ViewBag = Reserva de espaço para armazenar informações para recuperar na VIEW.
+
+            //ViewBag = tem a função de "carregar" as informações do Controller para a VIEW.
+
+            ViewBag.Equipes = equipeModel.LerEquipes();
+            return View();
+        }
+    }
+}
